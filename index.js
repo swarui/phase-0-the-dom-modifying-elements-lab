fa917f8137b1700c5abfaa447b508e61cdddf93f
stevewarui,@gmail.com <stevewarum@gmail.com>
2023-09-14 21:41:17 +0300

    Completed Assignment

--- index.js
+++ index.js
@@ -1,1 +1,10 @@
 // Write your code here!
+const main = document.getElementById('main')
+main.remove();
+
+const body = document.body
+
+const newHeader = document.createElement ('h1')
+newHeader.id = 'victory'
+newHeader.innerHTML = 'Steve Maina is the champion'
+body.appendChild(h1);
